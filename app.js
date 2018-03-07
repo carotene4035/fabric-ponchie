8eb3d71aafd79a0dc03dbe8a8525002f0de428db
carotene512 <karoten512@gmail.com>
2018-03-07 23:31:25 +0900

    クラスを使ったリファクタリング途中。

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ window.onload = function() {
   const download = document.getElementById("download");
   download.onclick = function() {
 
-
     /** canvasを画像ファイルにする */
     const ret = canvas.toDataURL({
       format: 'jpeg',
@@ -48,40 +47,61 @@ window.onload = function() {
     }
     return blob;
   }
+}
 
 
+/*
+ * class droparea
+ *   ファイルがドロップされたタイミングでcanvasに画像を生成する
+ */
+function DropArea(domElementId, canvas) {
+  this.initialize(domElement, canvas);
+  this.handleEvents();
+}
 
-  /** ドラッグ&ドロップで画像を追加 */
-  var droparea = document.getElementById('droparea');
+DropArea.prototype.initialize = function(domElement, canvas) {
+  this.canvas = canvas;
+  this.domElement = document.getElementById(domElementId);
+}
 
-  // dragoverイベントのリスナーを設定
-  droparea.addEventListener('dragover', function(evt) {
-    evt.preventDefault();
-  }, true);
+DropArea.prototype.handleEvents = function() {
+  const self = this;
 
-  // dropイベントのリスナーを設定
-  droparea.addEventListener('drop', function(evt) {
+  /** dropエリアにobjectがある */
+  this.domElement.addEventListener('dragover', function(e) {
+    self.domElement.classList.add('dragover');
+  });
+
+  /** dropエリアからobjectが離れた */
+  this.domElement.addEventListener('dragleave', function(e) {
+    self.domElement.classList.remove('dragover');
+  });
+
+  /** dropエリアにものをdragしたとき */
+  this.domElement.addEventListener('drop', function(evt) {
     // fileオブジェクトの取得
     const file = evt.dataTransfer.files[0];
-
     // file reader生成
     const reader = new FileReader();
 
+    const _self = self;
     reader.onload = function() {
+
       const image = new Image();
+      const __self = _self;
 
       /** image要素が読み込まれてからcanvasに追加 */
       image.onload = function() {
         /** image要素をcanvasに追加 */
-        var imgInstance = new fabric.Image(image, {
+        const imgInstance = new fabric.Image(image, {
           left: 10,
           top: 10,
           selectable: false
         });
-        canvas.add(imgInstance);
+        __self.canvas.add(imgInstance);
 
         /** 選択用の四角をcanvasに追加 */
-        var rect = new fabric.Rect({
+        const rect = new fabric.Rect({
           left: 100,
           top: 100,
           fill:'transparent',
@@ -90,18 +110,26 @@ window.onload = function() {
           stroke: '#f00',
           strokeWidth: 1
         });
-        canvas.add(rect);　
-        canvas.bringToFront(rect);
+        __self.canvas.add(rect);　
+        __self.canvas.bringToFront(rect);
       }
-
       /** image要素の読み込み */
       image.src = reader.result;
-
     };
     reader.readAsDataURL(file);
     evt.preventDefault();
-
   }, true);
 
 }
 
+const canvas = new fabric.Canvas('test_canvas');
+
+// drop areaのelementを指定する
+// classを作ってviewに割り込ませるのか。。。
+// 利点。別のviewに対して、同じ処理をさせることができる. 何度も同じものを書かなくてもよい
+const dropArea = new DropArea('droparea', canvas);
+
+// const saveButton= new SaveButton();
+
+// saveボタンクラスを作って処理をまとめる
+// そうか。。。こいつはcanvasを持っていないといけないのか。。。
